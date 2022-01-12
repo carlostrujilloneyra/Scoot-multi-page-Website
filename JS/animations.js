5703fdaae73fecdf6d2fd5eec2ebbcc2ec14741a
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-12 02:21:21 -0500

    Arreglando la altura de los textos y cambiando los tiempos a las animaciones con scroll

--- JS/animations.js
+++ JS/animations.js
@@ -4,25 +4,25 @@ window.sr = ScrollReveal();
 /*Animaciones para el index*/ 
 
 sr.reveal('.main-content--firstSection', {
-    duration: 1800,
+    duration: 1550,
     origin: 'left',
     distance:'350px'
 });
 
 sr.reveal('.main-content--secondSection', {
-    duration: 1800,
+    duration: 1550,
     origin: 'right',
     distance:'350px'
 });
 
 sr.reveal('.main-content--thirdSection', {
-    duration: 1800,
+    duration: 1550,
     origin: 'left',
     distance:'350px'
 });
 
 sr.reveal('.main-content--fourSection', {
-    duration: 1800,
+    duration: 1550,
     origin: 'right',
     distance:'350px'
 });
@@ -31,53 +31,72 @@ sr.reveal('.main-content--fourSection', {
 /*Animaciones para el about*/ 
 
 sr.reveal('.main-title-article-about', {
-    duration: 1800,
-    origin: 'top',
-    distance:'350px'
+    duration: 1550,
+    origin: 'right',
+    distance:'300px'
 });
 
 sr.reveal('.main-title-article-careers', {
-    duration: 1800,
-    origin: 'top',
+    duration: 1550,
+    origin: 'left',
     distance:'350px'
 });
 
 sr.reveal('.main-content-container__firstPart', {
-    duration: 1800,
+    duration: 1550,
     origin: 'left',
     distance:'350px'
 });
 
 sr.reveal('.main-content-container__secondPart', {
-    duration: 1800,
+    duration: 1550,
     origin: 'right',
     distance:'350px'
 });
 
+sr.reveal('.main-content-container__secondPart', {
+    duration: 1550,
+    origin: 'right',
+    distance:'350px'
+});
+
+sr.reveal('.main-content-values-about', {
+    duration: 1550,
+    origin: 'right',
+    distance:'350px'
+});
+
+
 /*Animaciones para el location*/ 
 
 sr.reveal('.main-content__section-location', {
-    duration: 2400,
+    duration: 1550,
     origin: 'bottom',
     distance:'200px'
 });
 
 sr.reveal('.main-content__secondPart', {
-    duration: 1800,
+    duration: 1550,
     origin: 'bottom',
     distance:'300px'
 });
 
 sr.reveal('.main-content-countries', {
-    duration: 1800,
+    duration: 1550,
     origin: 'bottom',
     distance:'400px'
 });
 
 /*Animaciones para careers*/
 
+sr.reveal('.main-content-values-careers', {
+    duration: 1550,
+    origin: 'left',
+    distance:'350px'
+});
+
 sr.reveal('.main-section-container-info', {
-    duration: 1800,
+    duration: 1550,
     origin: 'bottom',
     distance:'400px'
 });
