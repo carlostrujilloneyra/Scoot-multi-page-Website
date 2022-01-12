33023abd294e95be5051b7a6b502f98b932a1d55
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-11 22:33:55 -0500

    Se agrega el script para animaciones mediante el scroll

---
+++ JS/animations.js
@@ -0,0 +1,43 @@
+
+window.sr = ScrollReveal();
+
+/*Animaciones para el index*/ 
+
+sr.reveal('.main-content--firstSection', {
+    duration: 2800,
+    origin: 'left',
+    distance:'400px'
+});
+
+sr.reveal('.main-content--secondSection', {
+    duration: 2800,
+    origin: 'right',
+    distance:'400px'
+});
+
+sr.reveal('.main-content--thirdSection', {
+    duration: 2800,
+    origin: 'left',
+    distance:'400px'
+});
+
+sr.reveal('.main-content--fourSection', {
+    duration: 2800,
+    origin: 'right',
+    distance:'400px'
+});
+
+
+/*Animaciones para el about*/ 
+
+sr.reveal('.main-title-article-about', {
+    duration: 2000,
+    origin: 'top',
+    distance:'400px'
+});
+
+sr.reveal('.main-title-article-careers', {
+    duration: 2400,
+    origin: 'top',
+    distance:'400px'
+});
