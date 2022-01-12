01c91a515a2f90fbeba1cff1efa9eb32aaabd148
Carlos Trujillo <augusto21_20@hotmail.com>
2022-01-11 23:02:45 -0500

    Se añadió las animaciones de scroll para todos los archivos html

--- JS/animations.js
+++ JS/animations.js
@@ -4,27 +4,27 @@ window.sr = ScrollReveal();
 /*Animaciones para el index*/ 
 
 sr.reveal('.main-content--firstSection', {
-    duration: 2800,
+    duration: 2000,
     origin: 'left',
-    distance:'400px'
+    distance:'350px'
 });
 
 sr.reveal('.main-content--secondSection', {
-    duration: 2800,
+    duration: 2000,
     origin: 'right',
-    distance:'400px'
+    distance:'350px'
 });
 
 sr.reveal('.main-content--thirdSection', {
-    duration: 2800,
+    duration: 2000,
     origin: 'left',
-    distance:'400px'
+    distance:'350px'
 });
 
 sr.reveal('.main-content--fourSection', {
-    duration: 2800,
+    duration: 2000,
     origin: 'right',
-    distance:'400px'
+    distance:'350px'
 });
 
 
@@ -33,11 +33,51 @@ sr.reveal('.main-content--fourSection', {
 sr.reveal('.main-title-article-about', {
     duration: 2000,
     origin: 'top',
-    distance:'400px'
+    distance:'350px'
 });
 
 sr.reveal('.main-title-article-careers', {
-    duration: 2400,
+    duration: 2000,
     origin: 'top',
+    distance:'350px'
+});
+
+sr.reveal('.main-content-container__firstPart', {
+    duration: 2000,
+    origin: 'left',
+    distance:'350px'
+});
+
+sr.reveal('.main-content-container__secondPart', {
+    duration: 2000,
+    origin: 'right',
+    distance:'350px'
+});
+
+/*Animaciones para el location*/ 
+
+sr.reveal('.main-content__section-map', {
+    duration: 2400,
+    origin: 'bottom',
+    distance:'200px'
+});
+
+sr.reveal('.main-content__secondPart', {
+    duration: 2000,
+    origin: 'bottom',
+    distance:'300px'
+});
+
+sr.reveal('.main-content-countries', {
+    duration: 2000,
+    origin: 'bottom',
+    distance:'400px'
+});
+
+/*Animaciones para careers*/
+
+sr.reveal('.main-section-container-info', {
+    duration: 2000,
+    origin: 'bottom',
     distance:'400px'
 });
